2b38442bd7f0c8c659a288003fa44977e2defb84
dailiwang <758017188@qq.com>
2017-07-04 17:57:09 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+  	<!--头布局-->
+    <MyHead></MyHead>
+    <!--导航栏-->
+    <div class="tab">
+    	<div class="tab-item">商品</div>
+    	<div class="tab-item">评价</div>
+    	<div class="tab-item">商家</div>
+    </div>
+    <div id="content">主体内容</div>
+  </div>
+</template>
+
+<script>
+	import MyHead from './components/header/MyHead'
+	export default {
+		components: {
+			MyHead
+		}
+	};
+</script>
+
+<style>
+	#app {
+		
+	}
+	.tab{
+		display: flex;
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+	}
+	.tab .tab-item{
+		flex: 1;
+		text-align: center;
+	}
+</style>
---
+++ src/components/header/MyHead.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="head">
+		<h3>这是头布局</h3>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'head',
+		data(){
+			return {
+				msg: '这是data中的数据'
+			}
+		}
+	}
+</script>
+	
+<style>
+	#head {
+		border: 1px solid gray;
+	}
+</style>
